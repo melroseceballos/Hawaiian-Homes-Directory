5b2f10fcd542b6e561c276e2393c6d06e2017a73
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-16 09:00:33 -0700

    changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
